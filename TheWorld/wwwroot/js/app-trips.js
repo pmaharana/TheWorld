cdf826f6e783494f4e397d7f5033e7f6a5378745
Pranay Maharana <pranay.maharana@gmail.com>
2017-08-19 18:32:37 -0400

    Added angular, created validation form and set back dummy data from client side

---
+++ TheWorld/wwwroot/js/app-trips.js
@@ -0,0 +1,10 @@
+﻿//app-trips.js
+
+(function () {
+
+    "use strict";
+
+    // this is where the angular moduel is being created, due to the second parameter
+    angular.module("app-trips", []);
+
+})();
---
+++ TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,32 @@
+﻿//tripsController.js
+
+(function () {
+
+    "use strict";
+
+    //Getting the existing module
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    function tripsController() {
+
+        let vm = this; //this is the object that is being returned from the trips controller.
+                        //controller is going to call new trips controller, returns something like a class
+
+        vm.trips = [{
+            name: "US Trip",
+            created: new Date()
+         }, {
+            name: "World Trip",
+            created: new Date()
+        }];
+
+        vm.newTrip = {};
+
+        vm.addTrip = function () {
+            vm.trips.push({ name: vm.newTrip.name, created: new Date() });
+            vm.newTrip = {};
+        };
+    }
+
+})();

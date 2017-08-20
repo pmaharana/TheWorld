f8a9b3c67806d08d2ab809b4954bc2badf44a435
Pranay Maharana <pranay.maharana@gmail.com>
2017-08-19 22:04:32 -0400

    more routing views

--- TheWorld/wwwroot/js/app-trips.js
+++ TheWorld/wwwroot/js/app-trips.js
@@ -5,6 +5,17 @@
     "use strict";
 
     // this is where the angular moduel is being created, due to the second parameter
-    angular.module("app-trips", ["simpleControls"]);
+    angular.module("app-trips", ["simpleControls", "ngRoute"])
+        .config(function ($routeProvider) {
+
+            $routeProvider.when("/", {
+                controller: "tripsController",
+                controllerAs: "vm",
+                templateUrl: "/views/tripsView.html"
+            });
+
+            $routeProvider.otherwise({ redirectTo: "/" });
+
+        });
 
 })();
